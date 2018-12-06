dd6598cd1fe90695844d211a8b46fd9107fe0e55
Corbaxhi <kcorbaxh@umich.edu>
2018-12-06 18:04:39 -0500

    First commit

---
+++ testSelectionSort.java
@@ -0,0 +1,126 @@
+import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class testSelectionSort {
+      @Test
+
+      public void test() {
+            testPositive();
+            testNegative();
+            testMixed();
+            testDuplicates();
+      }
+
+       public testSelectionSort() {
+
+       }
+       
+       public void testPositive(){
+             int[] arr = new int[5];
+
+       arr[0] = 8;
+       arr[1] = 9;
+       arr[2] = 7;
+       arr[3] = 10;  
+       arr[4] = 2;
+
+       
+
+       int[] Sortedarr = new int[5];
+
+       Sortedarr[0] = 2;  
+       Sortedarr[1] = 7;
+       Sortedarr[2] = 8;
+       Sortedarr[3] = 9;
+       Sortedarr[4] = 10;
+
+      
+       SelectionSort arr1 = new SelectionSort();
+       arr = arr1.basicSelectionSort(arr);
+
+       assertArrayEquals(Sortedarr, arr);
+
+       }
+
+       
+
+       public void testNegative(){
+
+             int[] arr = new int[5];
+
+       arr[0] = -8;
+       arr[1] = -9;
+       arr[2] = -7;
+       arr[3] = -10;  
+       arr[4] = -2;
+
+       
+
+       int[] Sortedarr = new int[5];
+
+       Sortedarr[0] = -10;  
+       Sortedarr[1] = -9;
+       Sortedarr[2] = -8;
+       Sortedarr[3] = -7;
+       Sortedarr[4] = -2;
+
+       SelectionSort arr1 = new SelectionSort();
+       arr = arr1.basicSelectionSort(arr);
+
+       assertArrayEquals(Sortedarr, arr);
+
+       }  
+
+       public void testMixed(){
+
+             int[] arr = new int[5];
+
+       arr[0] = -8;
+       arr[1] = 9;
+       arr[2] = -7;
+       arr[3] = 10;  
+       arr[4] = 0;
+
+       
+
+       int[] Sortedarr = new int[5];
+
+       Sortedarr[0] = -8;  
+       Sortedarr[1] = -7;
+       Sortedarr[2] = 0;
+       Sortedarr[3] = 9;
+       Sortedarr[4] = 10;
+
+       SelectionSort arr1 = new SelectionSort();
+       arr = arr1.basicSelectionSort(arr);
+
+       assertArrayEquals(Sortedarr, arr);
+
+       }
+       
+       public void testDuplicates(){
+
+             int[] arr = new int[5];
+
+       arr[0] = 8;
+       arr[1] = 9;
+       arr[2] = 8;
+       arr[3] = 10;  
+       arr[4] = 4;
+
+       int[] Sortedarr = new int[5];
+
+       Sortedarr[0] = 4;  
+       Sortedarr[1] = 8;
+       Sortedarr[2] = 8;
+       Sortedarr[3] = 9;
+       Sortedarr[4] = 10;
+       
+       SelectionSort arr1 = new SelectionSort();
+       arr = arr1.basicSelectionSort(arr);    
+
+       assertArrayEquals(Sortedarr, arr);
+
+       }
+}
